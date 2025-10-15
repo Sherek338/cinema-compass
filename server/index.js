6fa09c827d092b06a4b803f347f5c21415d46bbd
Sherek338 <dante.ocylus.js@gmail.com>
2025-10-15 19:31:22 +0500

    vercel fix

--- server/index.js
+++ server/index.js
@@ -7,12 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
 
 module.exports = app;
