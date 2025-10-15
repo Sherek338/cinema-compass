8258ecfe02272184a2b0b934d45a0b3fcd3a3976
Sherek338 <dante.ocylus.js@gmail.com>
2025-10-15 18:52:42 +0500

    vercel initialization

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = app;
