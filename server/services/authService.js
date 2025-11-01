96cea32864665586fa8c4350efd5742739ed0a06
Sherek338 <dante.ocylus.js@gmail.com>
2025-11-01 12:17:58 +0500

    services structure changes

--- server/services/authService.js
+++ server/services/authService.js
@@ -6,53 +6,57 @@ const { validateUsername, validateEmail } = require('../libs/utils');
 const tokenService = require('./tokenService');
 const mailService = require('./mailService');
 
-class AuthService {
-  async registration(username, email, password) {
-    if (!validateUsername(username) || !validateEmail(email) || !password) {
-      throw new Error('All fields are required');
-    }
-    const userExists = await UserModel.findOne({ email });
-    if (userExists) {
-      throw new Error('User with this email already exists');
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const activationLink = uuid.v4();
-
-    const newUser = await UserModel.create({
-      username,
-      email,
-      password: hashedPassword,
-      activationLink,
-    });
-
-    await mailService.sendActivationLink(
-      email,
-      `${process.env.API_URL}/api/activate/${activationLink}`
-    );
-
-    const userDTO = new UserDTO(newUser);
-    const tokens = tokenService.generateTokens({ ...userDTO });
-    await tokenService.saveToken(userDTO.id, tokens.refreshToken);
-
-    return { ...tokens, user: userDTO };
+const registration = async (username, email, password) => {
+  if (!validateUsername(username) || !validateEmail(email) || !password) {
+    throw new Error('All fields are required');
   }
-  async login(email, password) {
-    if (validateEmail(email) || !password) {
-      throw new Error('All fields are required');
-    }
+  const userExists = await UserModel.findOne({ email });
+  if (userExists) {
+    throw new Error('User with this email already exists');
+  }
 
-    //TODO
-  }
-  async logout(refreshToken) {
-    // Logic for user logout
-  }
-  async activate(activationLink) {
-    // Logic for account activation
-  }
-  async refresh(refreshToken) {
-    // Logic for token refresh
-  }
-}
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const activationLink = uuid.v4();
 
-module.exports = new AuthService();
+  const newUser = await UserModel.create({
+    username,
+    email,
+    password: hashedPassword,
+    activationLink,
+  });
+
+  await mailService.sendActivationLink(
+    email,
+    `${process.env.API_URL}/api/activate/${activationLink}`
+  );
+
+  const userDTO = new UserDTO(newUser);
+  const tokens = tokenService.generateTokens({ ...userDTO });
+  await tokenService.saveToken(userDTO.id, tokens.refreshToken);
+
+  return { ...tokens, user: userDTO };
+};
+const login = async (email, password) => {
+  if (validateEmail(email) || !password) {
+    throw new Error('All fields are required');
+  }
+
+  //TODO
+};
+const logout = async (refreshToken) => {
+  // Logic for user logout
+};
+const activate = async (activationLink) => {
+  // Logic for account activation
+};
+const refresh = async (refreshToken) => {
+  // Logic for token refresh
+};
+
+module.exports = {
+  registration,
+  login,
+  logout,
+  activate,
+  refresh,
+};
